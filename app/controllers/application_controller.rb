f4b68900e79ec88d213ce97d05b48d6080d2b217
umamahe <umamahe1007@gmail.com>
2010-09-04 10:22:11 +0530

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::Base
+  before_filter :set_locale 
+  protect_from_forgery
+
+
+  def set_locale 
+# if params[:locale] is nil then I18n.default_locale will be used  
+I18n.locale = params[:locale] 
+end 
+
+
+def default_url_options(options={}) 
+ logger.debug "default_url_options is passed options: #{options.inspect}\n"  
+{ :locale => I18n.locale } 
+end 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f590b48d353d82dd9113c53ffaac41af46da8dd1e92fab3d5a7fd42fa1f0685769302d05abf3cce07609aa3c737c73ba77a45c67a9076e1af0a57e1d8bfdd8b3'
